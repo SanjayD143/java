e33eb26adfa4199e26c12562a6c2b64742f619c7
Sanjay D <sanjayd143.xworkz@gmail.com>
2025-08-08 16:28:02 +0530

    programme

---
+++ Cricket.java
@@ -0,0 +1,36 @@
+class Cricket{
+public static int numberOfPlayer(){
+int number =11;
+return number;
+
+
+}
+public static int teamsNames(){
+int teams = 10;
+return teams;
+		
+}
+public static int overs(){
+int balls = 120;
+return balls;	
+		
+}
+
+public static int days(){
+int stump = 5;
+return stump;
+}
+	
+public static int runs(){
+int wickets =7;
+return wickets;	
+	
+	
+}	
+
+
+
+
+
+
+}
