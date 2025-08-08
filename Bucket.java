4460d86b68802a618d1e5e85b96ec87ac2bbff01
Sanjay D <sanjayd143.xworkz@gmail.com>
2025-08-08 16:25:01 +0530

    programme

---
+++ Bucket.java
@@ -0,0 +1,21 @@
+class Bucket{
+public static void quality(){
+System.out.println("types of buckets");
+}
+public static void price(){
+System.out.println("types of cost");
+quality();
+}
+
+public static void length(){
+System.out.println("9.555");
+price();
+
+
+}
+
+
+}
+
+
+
