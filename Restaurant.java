4b3c24cf1509abe4444b5ad4dd0f47b283f1559a
Sanjay D <sanjayd143.xworkz@gmail.com>
2025-08-20 21:21:58 +0530

    sanjay

---
+++ Restaurant.java
@@ -0,0 +1,18 @@
+class Restaurant{
+public static void price(int itemName){
+
+int itemPrice =0;
+if(itemName==1){
+   itemPrice=654;
+   
+   System.out.println("restaurant items available   "+itemPrice );
+   
+}else{
+
+System.out.println("restaurant branches");
+
+}
+
+}
+
+}
