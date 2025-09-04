40459080dc365d993b4fcde761b19b9a347dfaef
Sanjay D <sanjayd143.xworkz@gmail.com>
2025-09-04 16:02:36 +0530

    practice

---
+++ practice/Rain.java
@@ -0,0 +1,12 @@
+class Rain {
+    public void temperature() {
+        int temp = 28;
+        System.out.println("Temperature: " + temp + "°C");
+    }
+
+    public void hours() {
+        int hrs = 5;
+        System.out.println("Rain hours: " + hrs);
+    }
+}
+
---
+++ practice/RainRunner.java
@@ -0,0 +1,8 @@
+class RainRunner {
+    public static void main(String []args) {
+        Rain rain = new Rain(); 
+        rain.temperature();      
+        rain.hours();
+        System.out.println("--------------");
+    }
+}
