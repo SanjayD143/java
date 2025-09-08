6a1f01a4e78457b3de13d1f9b7cd1e9a7d5f13df
Sanjay D <sanjayd143.xworkz@gmail.com>
2025-09-08 21:47:18 +0530

    instance variable

---
+++ re-initialize/Bulb.java
@@ -0,0 +1,22 @@
+class Bulb{
+public int price;
+
+public void noOfBulb(){
+int amount=321;
+long bulbNumber=9542153;
+char rate ='A';
+float bulbHeigth=5.6f;
+boolean lightBulb=true;	
+double bulbWeight =	1.22;
+String bulbName="magaBulb";
+
+System.out.println("bulb lists "+price);	
+	
+	
+	
+	
+}
+
+
+
+}
---
+++ re-initialize/BulbRunner.java
@@ -0,0 +1,10 @@
+class BulbRunner{
+public static void main(String []args){
+int	price=200;
+System.out.println("this is re initialize " +price);
+Bulb bulb=new Bulb();
+bulb.noOfBulb();
+}
+
+
+}
